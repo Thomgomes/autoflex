0715fecb74a75e3be5932547ecbbdd9be114bd5c
ThomGomes <math.011@hotmail.com>
2026-02-15 04:41:09 -0300

    products slice fix

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productionReducer from "./slices/productionSlice.ts";
 import materialReducer from "./slices/materialSlice.ts";
+import productReducer from "./slices/productSlice.ts";
 
 export const store = configureStore({
   reducer: {
     production: productionReducer,
     material: materialReducer,
+    product: productReducer,
   },
 });
 
